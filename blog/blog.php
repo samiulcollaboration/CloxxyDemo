f5920828a8926365bec35c4606fea52020d65110
rakib <rakibrk1811@gmail.com>
2019-10-12 15:26:08 +0530

    added dynamic features to blog content

--- blog/blog.php
+++ blog/blog.php
@@ -8,8 +8,19 @@
     include('blog-header.php'); 
     
     if ($post_result->num_rows > 0) {
-            while($row = $post_result->fetch_assoc()) {
-                echo $row['content'];
+            while($row = $post_result->fetch_assoc()) { ?>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <h2 class="blog-title"><?php echo $row['title']; ?></h2>
+                            <img class="blog-thumbnail" src="<?php echo $row['thumbnail']; ?>" alt="">
+                            <div class="blog-content"><?php echo $row['content']; ?></div>
+                        </div>
+                    </div>
+                </div>
+
+                
+        <?php
             }
         } ?>
 
